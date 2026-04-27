b8f9fb1df13755bf3d787662aa0875bbb6a1d19a
leonardo28-dev <leonardo.wsilva@sptech.school>
2026-04-27 20:22:06 -0300

    feat Projeto atualizado

--- src/models/quizModel.js
+++ src/models/quizModel.js
@@ -6,10 +6,30 @@ function salvar(acertos, erros, fk_usuario) {
         VALUES (${acertos}, ${erros}, ${fk_usuario});
     `;
 
-    console.log("Executando SQL: " + instrucaoSql);
+    console.log("Executando a instrução SQL:\n " + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
     salvar
 };
+
+/*
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, email, senha,cpf) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha,cpf);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (nome, email, senha,cpf) VALUES ('${nome}', '${email}', '${senha}','${cpf}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
+*/
--- src/routes/quiz.js
+++ src/routes/quiz.js
@@ -3,8 +3,31 @@ var router = express.Router();
 
 var quizController = require("../controllers/quizController");
 
-router.post("/salvar", function (req, res) {
+router.post("/salvar",function(req, res) {
     quizController.salvar(req, res);
 });
 
 module.exports = router;
+
+
+/*
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.autenticar(req, res);
+});
+
+module.exports = router;
+
+
+
+
+*/

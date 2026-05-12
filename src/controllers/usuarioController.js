a5460114b56e12cbd683cb97225807a3d2d37b54
leonardo28-dev <leonardo.wsilva@sptech.school>
2026-05-12 11:20:16 -0300

    Subindo comentarios do projeto

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -21,7 +21,7 @@ function autenticar(req, res) {
                         console.log(resultadoAutenticar);
 
 
-                        // //  Aqui eu estou enviando os dados do usuário direto para o navegado, sem tentar busacar aquários
+                        // //  Aqui eu estou enviando os dados do usuário direto para o navegador.
                         res.json({
                             id: resultadoAutenticar[0].id,
                             email: resultadoAutenticar[0].email,
@@ -46,14 +46,14 @@ function autenticar(req, res) {
     }
 }
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    // Variáveis que vão recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
     var cpf = req.body.cpfServer;
 
 
-    // Faça as validações dos valores
+    // Validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
@@ -66,7 +66,7 @@ function cadastrar(req, res) {
 
     } else {
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        // Aqui estou passando os valores como parâmetro  aonde vão para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nome, email, senha, cpf)
             .then(
                 function (resultado) {
